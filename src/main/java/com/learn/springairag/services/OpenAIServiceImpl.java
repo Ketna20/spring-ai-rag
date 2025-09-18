fc4505ca4e0f279a68073fa778512cf02369a004
ketna20 <ketna.kumar@gmail.com>
2025-09-17 20:40:34 -0700

    added embedding model in application.yaml

--- src/main/java/com/learn/springairag/services/OpenAIServiceImpl.java
+++ src/main/java/com/learn/springairag/services/OpenAIServiceImpl.java
@@ -31,7 +31,7 @@ public class OpenAIServiceImpl implements OpenAIService{
     @Override
     public Answer getAnswer(Question question) {
         List<Document> documents = vectorStore.similaritySearch(SearchRequest.builder()
-                .query(question.question()).topK(5).build());
+                .query(question.question()).topK(4).build());
         List<String> contentList = documents.stream().map(Document::getContent).toList();
 
         PromptTemplate promptTemplate = new PromptTemplate(ragPromptTamplate);
